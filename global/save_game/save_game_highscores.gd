9d75e73e3dbdcbd3ae00ad0a2877d09569671881
Acemany <116539353+acemany@users.noreply.github.com>
2024-07-28 03:01:43 +0300

    fix highscores

--- global/save_game/save_game_highscores.gd
+++ global/save_game/save_game_highscores.gd
@@ -5,16 +5,8 @@ extends Resource
 
 
 func try_submit(data: LevelData, meters: float) -> void:
-	var id: int = data.id
-	if id not in highscores:
-		highscores[id] = meters
-	else:
-		var current_highscore: float = highscores[id] as float
-		highscores[id] = maxf(current_highscore, meters)
+	highscores[data.id] = maxf(highscores[data.id], meters)
 
 
 func get_highscore(data: LevelData) -> float:
-	var id: int = data.id
-	if id not in highscores:
-		return 0.0
-	return highscores[id] as float
+	return highscores[data.id]
